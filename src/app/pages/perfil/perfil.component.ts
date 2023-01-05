2024227197b60fd7f4a3824eb53186b5eb3fc041
Milton <gmraivan@gmail.com>
2023-01-05 13:19:57 -0500

    Se avanzo  con el perfil

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/rpt.listarUsuarios';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  public usuario: any;
+  public campoEmail: boolean = false
+  public campoNombre: boolean = false
+  public campoTelefono: boolean = false
+  public campoAfter: string = ''
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
+    // this.cargarUsuario()
+    this.usuario = this.usuarioService.usuario
   }
+  mostrarCampo(usuario: Usuario, campo: string) {
+    if (campo == 'email') {
+      this.campoEmail = true
+    } else if (campo == 'nombre') {
+      this.campoNombre = true
+    } else if (campo == 'telefono') {
+      this.campoTelefono = true
+      console.log(this.campoTelefono)
+    }
 
+  }
+  guardarCampo(usuario:Usuario,campo: string) {
+    if (campo == 'email') {
+      this.campoEmail = false
+    } else if (campo == 'nombre') {
+      this.campoNombre = false
+    } else if (campo == 'telefono') {
+      this.campoTelefono = false
+    }
+    this.campoEmail=false
+  }
 }
